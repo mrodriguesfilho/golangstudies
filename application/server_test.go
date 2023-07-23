f9e5defc3c44f1cfbcf4f53542795089c9334fd9
MRF <54534037+mrodriguesfilho@users.noreply.github.com>
2023-07-23 18:23:33 -0300

    just an impl for saving files on a json db

--- application/server_test.go
+++ application/server_test.go
@@ -183,7 +183,7 @@ func assertResponseBody(t testing.TB, got, want string) {
 type StubPlayerStore struct {
 	scores   map[string]int
 	winCalls []string
-	league   []Player
+	league   League
 }
 
 func (s *StubPlayerStore) GetPlayerScore(name string) int {
@@ -195,6 +195,6 @@ func (s *StubPlayerStore) RecordWin(name string) {
 	s.winCalls = append(s.winCalls, name)
 }
 
-func (s *StubPlayerStore) GetLeague() []Player {
+func (s *StubPlayerStore) GetLeague() League {
 	return s.league
 }
