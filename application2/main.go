e1811f5e1c829e0d09de4e1b910413d126790e4a
MRF <54534037+mrodriguesfilho@users.noreply.github.com>
2023-07-23 23:54:14 -0300

    abstracted away database from main.go

--- application2/main.go
+++ application2/main.go
@@ -1,44 +1,17 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"gitbook/application2/internal/infra/database"
 	"gitbook/application2/internal/usecases"
-	"log"
 
 	_ "github.com/lib/pq"
-	"github.com/spf13/viper"
 )
 
 func main() {
-	viper.SetConfigName("config")
-	viper.SetConfigType("json")
-	viper.AddConfigPath(".")
+	dbConnection := Init()
+	defer dbConnection.Close()
 
-	if err := viper.ReadInConfig(); err != nil {
-		log.Fatal("Error reading config file:", err)
-	}
-
-	host := viper.GetString("database.host")
-	port := viper.GetInt("database.port")
-	dbname := viper.GetString("database.dbname")
-	user := viper.GetString("database.user")
-	password := viper.GetString("database.password")
-	sslmode := viper.GetString("database.sslmode")
-
-	connectionString := fmt.Sprintf("host=%s port=%d dbname=%s user=%s password=%s sslmode=%s",
-		host, port, dbname, user, password, sslmode)
-
-	db, err := sql.Open("postgres", connectionString)
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer db.Close()
-
-	orderRepository := database.NewOrderRepository(db)
+	orderRepository := database.NewOrderRepository(dbConnection)
 	usecase := usecases.NewCalculateFinalPrice(orderRepository)
 
 	input := usecases.OrderInput{
